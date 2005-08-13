9314b4c04cf043d54fe123b93d1d87b95ab6c56d
lightbulbs <lightbulbs@662d3e3c-bab5-4bb8-927d-f9ea1456bd5c>
2005-08-13 04:37:43 +0000

    Initial check-in

---
+++ AMSProgressBar/AMSProgressPanel.h
@@ -0,0 +1,49 @@
+//==============================================================================
+//
+// File:		AMSProgressPanel.h
+//
+// Purpose:		Displays a progress bar which estimates the time remaining to 
+//				completion.
+//
+//  Created by Allen Smith on Sun Sept 19 2004.
+//  Copyright (c) 2004. All rights reserved.
+//==============================================================================
+#import <Cocoa/Cocoa.h>
+
+@interface AMSProgressPanel : NSObject
+{
+    IBOutlet NSWindow				*dialogWindow;
+    IBOutlet NSTextField			*explanatoryText;
+    IBOutlet NSProgressIndicator	*progressBar;
+    IBOutlet NSTextField			*timeRemaining;
+	
+	double			 progressAmount; //the value of the progress bar.
+	NSDate			*startTime; //when the progress bar was started; used to estimate time remaining
+	NSDate			*previousUpdateTime; //time at which the progress bar was last changed.
+	BOOL			 endsIndeterminate;
+	BOOL			 runningAsSheet; //if yes, the progress panel is currently onscreen as a sheet.
+	NSTimeInterval	 timeBetweenUpdates; //number of seconds between progress updates.
+}
+
+//Initialization
++ (AMSProgressPanel *) progressPanel;
++ (AMSProgressPanel *) doProgressBarWithMax:(float)maximum forWindow:(NSWindow *)parentWindow message:(NSString*)messageKey;
+- (id) initWithMax:(float)maximum message:(NSString*)messageKey;
+
+//Accessors
+- (void) setValue:(float)newValue;
+- (void) increment;
+- (void) setMinValue:(double)newValue;
+- (void) setMaxValue:(double)newValue;
+- (void) setMessage:(NSString *)message;
+- (void) setShowsTimeRemaining:(BOOL)flag;
+- (void) setBecomesIndeterminateWhenCompleted:(BOOL)flag;
+- (void) setUpdateFrequency:(NSTimeInterval)updateInterval;
+
+//Actions
+- (void) showProgressPanel;
+- (void) showAsSheetForWindow:(NSWindow *)parentWindow;
+- (void) close;
+
+
+@end

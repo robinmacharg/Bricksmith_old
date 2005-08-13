08fe97f8a4eb5cae8c7c7f8b6166c349424d2545
lightbulbs <lightbulbs@662d3e3c-bab5-4bb8-927d-f9ea1456bd5c>
2005-08-13 04:37:43 +0000

    Initial check-in

---
+++ trunk/AMSProgressBar/AMSProgressBar.h
@@ -0,0 +1,11 @@
+/*
+ *  AMSProgressBar.h
+ *  AMSProgressBar
+ *
+ *  Created by Allen Smith on 2/16/05.
+ *  Copyright 2005 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <AMSProgressBar/AMSProgressPanel.h>
+
